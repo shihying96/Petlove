5b2194e58edd27957bb2803ba1e499ecdd2bf0cf
shihying96 <80135963+shihying96@users.noreply.github.com>
2021-04-15 17:07:26 +0800

    0415

--- js/index/web/custom.js
+++ js/index/web/custom.js
@@ -1,4 +1,24 @@
 $('.product_wrapAll').mouseenter(function () {
-    $('.disableBox1').css('margin-top', '100px').css('margin-left', '280px');
+    $('.disableBox1').css('margin-top', '-12px').css('margin-left', '252px').css('transition', '0.8s');
 
 })
+
+
+$('.product_wrapAll').mouseleave(function () {
+    $('.disableBox1').css('margin-top', '-500px').css('margin-left', '252px').css('transition', '1.2s');
+
+})
+
+
+
+
+// $('.product_wrapAll2').mouseenter(function () {
+//     $('.disableBox2').css('margin-top', '-12px').css('margin-left', '450px').css('transition', '0.8s');
+
+// })
+
+
+// $('.product_wrapAll2').mouseleave(function () {
+//     $('.disableBox2').css('margin-top', '-500px').css('margin-left', '450px').css('transition', '1.2s');
+
+// })
